caf3c6395d6074b6c49954fca4964baf50e8c21e
victorocna <victorocnarescu@gmail.com>
2019-10-22 17:04:11 +0300

    fix notification message

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express()
 const port = 3000
 const routes = require("./routes")
 
+app.use(helmet())
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
   return res.send(404)
 })
@@ -15,13 +19,11 @@ for (let i in routes) {
     slack.chat.postMessage({
       token: process.env.SLACK_API_TOKEN,
       channel: "general",
-      text: "Hello world from Node. Payload: " + JSON.stringify(req.body) || "none",
+      text: "Hello world from Node. Payload: " + (JSON.stringify(req.body) || "none"),
     })
 
     return res.send("Ok")
   })
 }
 
-app
-  .use(helmet())
-  .listen(port, () => console.log(`Chesscoders webhook app listening on port ${port}`))
+app.listen(port, () => console.log(`Chesscoders Webhook app listening on port ${port}`))
